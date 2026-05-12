b5132f6efa67fdbcc23b02e6b24eeeca84e89012
ZeroVN FX <huydre37@gmail.com>
2026-05-12 15:59:31 +0700

    feat/#product: exception handing using rpc exception

--- libs/interceptors/src/lib/tcpLogging.interceptor.ts
+++ libs/interceptors/src/lib/tcpLogging.interceptor.ts
@@ -1,6 +1,10 @@
-import { CallHandler, ExecutionContext, Injectable, Logger, NestInterceptor } from '@nestjs/common';
+import { CallHandler, ExecutionContext, HttpStatus, Injectable, Logger, NestInterceptor } from '@nestjs/common';
+import { catchError } from 'rxjs';
 import { Observable } from 'rxjs/internal/Observable';
 import { tap } from 'rxjs/internal/operators/tap';
+import { RpcException } from '@nestjs/microservices';
+import { HTTP_MESSAGE } from '@common/constants/http-message.enum';
+
 
 @Injectable()
 export class TcpLoggingInterceptor implements NestInterceptor {
@@ -20,7 +24,22 @@ export class TcpLoggingInterceptor implements NestInterceptor {
             tap((data) => {
                 const duration = new Date().getTime() - now.getTime();
                 Logger.log(`TCP >> End process '${processId}' >> Method: ${handlerName} >> at: ${new Date().toISOString()} >> duration: ${duration}ms >> output: ${JSON.stringify(data)}`);
-            }),
+            }
+            ),
+            catchError((error) => {
+                const duration = Date.now() - now.getTime();
+                Logger.error(
+                    `TCP >> Error process '${processId}': ${error.message} >> data: ${JSON.stringify(
+                        error,
+                    )}, after: '${duration}'ms`
+                );
+
+                throw new RpcException({
+                    code: error.status || error.code || error.error?.code || HttpStatus.INTERNAL_SERVER_ERROR,
+                    message: error?.response?.message || error?.message || HTTP_MESSAGE.INTERNAL_SERVER_ERROR,
+
+                })
+            })
         )
     }
 }
